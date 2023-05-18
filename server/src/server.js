fe6b90fbcdcb87e46dd5737ee891846da9f89ded
Daniel <tedomi999@gmail.com>
2023-05-18 20:48:24 +0700

    #3 - chore: split app.js from server.js. All req, event will be handle in app.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,8 @@
-import express from "express";
-import morgan from "morgan";
-import configViewEngine from "./configs/viewEngines.js";
-import initWebRoute from "./route/web.js";
-
+import createApp from "./app.js";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-let app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+const app = createApp();
 
-// Logging
-// app.use(morgan("common"));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// setup view engine
-configViewEngine(app);
-
-// Routing
-initWebRoute(app);
-
-app.listen(port, () => console.log(`Server is listening on port: ${port}`));
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
