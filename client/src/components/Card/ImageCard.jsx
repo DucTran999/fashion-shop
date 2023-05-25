90830c2b8d5719dd60c32ec755334bb53ec6a1e0
Ductran <tranaduc9x@gmail.com>
2023-05-25 18:54:07 +0700

    #8 - bug: hover on image is not show the Buy Now message

--- client/src/components/Card/ImageCard.jsx
+++ client/src/components/Card/ImageCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./ImageCard.module.scss";
@@ -6,14 +6,18 @@ import styles from "./ImageCard.module.scss";
 const cx = classNames.bind(styles);
 
 function ImageCard({ linkTo, imageUrl, cardStyle, title, titleStyle }) {
+  const [isShown, setIsShown] = useState(false);
+
   return (
     <div className={cx("card__wrap")}>
       <Link
         to={linkTo}
         className={cx(cardStyle)}
         style={{ backgroundImage: `url(${imageUrl})` }}
+        onMouseEnter={() => setIsShown("active")}
+        onMouseLeave={() => setIsShown("inactive")}
       ></Link>
-      <Link to={linkTo} className={cx(titleStyle)}>
+      <Link to={linkTo} className={cx(titleStyle, `${isShown}`)}>
         {title}
       </Link>
     </div>
