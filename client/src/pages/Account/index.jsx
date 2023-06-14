7bba02f0655d522a65b46b4391a7487e7e8759d3
Ductran <tranaduc9x@gmail.com>
2023-06-14 22:23:57 +0700

    #24 - chore: new announcement when user profile missing some information

--- client/src/pages/Account/index.jsx
+++ client/src/pages/Account/index.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
 
 import useWindowDimension from "../../hooks/useWindowDimension";
+import { updateSidebarSelection } from "../../features/activeNav/navAction";
+import { formatCapitalize } from "../../utils/formatData";
+
 import ViewDesktop from "./ViewDesktop";
 import ViewTablet from "./ViewTablet";
 import ViewMobile from "./ViewMobile";
@@ -11,7 +15,20 @@ import style from "./Account.module.scss";
 const cx = classNames.bind(style);
 
 const Account = () => {
+  document.title = formatCapitalize("Personal Account");
+
+  const isMounted = useRef(false);
+  const dispatch = useDispatch();
   const { width } = useWindowDimension();
+
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      updateSidebarSelection("Public profile", dispatch);
+    }
+
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <main className={cx("theme")}>
