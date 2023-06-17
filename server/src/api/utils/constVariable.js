265924979e40b5b93411160b4c29526feb9da950
Daniel <tedomi999@gmail.com>
2023-06-17 11:26:41 +0700

    #27 - feat: add func for filtering variants id and mapping order state

---
+++ server/src/api/utils/constVariable.js
@@ -0,0 +1,13 @@
+const MAGIC_NUMBER = {
+  limitRows: 16,
+};
+
+const ORDER_STATE_ID = {
+  pending: "1",
+  shipping: "2",
+  cancelled: "3",
+  completed: "4",
+  cancelling: "5",
+};
+
+export { MAGIC_NUMBER, ORDER_STATE_ID };
