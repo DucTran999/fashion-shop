276230e8ccf49e3ba15640c2ad7b57f91ff842f5
Ductran <tranaduc9x@gmail.com>
2023-06-17 10:12:10 +0700

    #23 - feat: new redux store for manage order, update new route to order page

--- admin/src/index.js
+++ admin/src/index.js
@@ -7,11 +7,9 @@ import AuthProvider from "./context/AuthProvider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <GlobalStyle>
-        <App />
-      </GlobalStyle>
-    </AuthProvider>
-  </React.StrictMode>
+  <AuthProvider>
+    <GlobalStyle>
+      <App />
+    </GlobalStyle>
+  </AuthProvider>
 );
