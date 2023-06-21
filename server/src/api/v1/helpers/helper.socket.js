066382356a37cbe3bb70858e1bbdc7eaeeda29a4
Daniel <tedomi999@gmail.com>
2023-06-21 23:27:53 +0700

    #38 - fix: catch error when retrieve data from redis fail

--- server/src/api/v1/helpers/helper.socket.js
+++ server/src/api/v1/helpers/helper.socket.js
@@ -2,37 +2,57 @@ import redisClient from "./init.redis.client.js";
 
 // Clear all user socket when server down
 const clearAllUserSocketIds = async () => {
-  const keys = await redisClient.keys("sockets:user#*");
+  try {
+    const keys = await redisClient.keys("sockets:user#*");
 
-  if (keys.length > 0) {
-    await redisClient.del(...keys);
+    if (keys.length > 0) {
+      await redisClient.del(...keys);
+    }
+  } catch (error) {
+    console.log("Server Internal Error");
   }
 };
 
 // When user open new tab
 const addOneSocketId = async (socketId, userId) => {
-  const key = `sockets:user#${userId}`;
-  await redisClient.sAdd(key, socketId);
+  try {
+    const key = `sockets:user#${userId}`;
+    await redisClient.sAdd(key, socketId);
+  } catch (error) {
+    console.log("Server Internal Error");
+  }
 };
 
 // When user close a tab
 const delOneSocketId = async (socketId, userId) => {
-  const key = `sockets:user#${userId}`;
-  await redisClient.sRem(key, socketId);
+  try {
+    const key = `sockets:user#${userId}`;
+    await redisClient.sRem(key, socketId);
+  } catch (error) {
+    console.log("Server Internal Error");
+  }
 };
 
 // when server want to contact to every tab. Like sent message, notifications.
 const getUserSocketIds = async (userId) => {
-  const key = `sockets:user#${userId}`;
-  const ids = await redisClient.sMembers(key);
+  try {
+    const key = `sockets:user#${userId}`;
+    const ids = await redisClient.sMembers(key);
 
-  return ids;
+    return ids;
+  } catch (error) {
+    console.log("Server Internal Error");
+  }
 };
 
 // when user logout remove all socket id.
 const delUserSocketIds = async (userId) => {
-  const key = `sockets:user#${userId}`;
-  await redisClient.del(key);
+  try {
+    const key = `sockets:user#${userId}`;
+    await redisClient.del(key);
+  } catch (error) {
+    console.log("Server Internal Error");
+  }
 };
 
 export {
