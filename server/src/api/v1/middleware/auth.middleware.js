6fad3d70ed5e943d1b3a589a14fb0f27e4655c96
Daniel <tedomi999@gmail.com>
2023-07-27 10:12:28 +0700

    add docstring for auth middleware

--- server/src/api/v1/middleware/auth.middleware.js
+++ server/src/api/v1/middleware/auth.middleware.js
@@ -5,6 +5,10 @@ import redisClient from "../helpers/init.redis.client.js";
 
 dotenv.config();
 
+// short form
+const rtKey = process.env.REFRESH_TOKEN_SECRET;
+const atKey = process.env.ACCESS_TOKEN_SECRET;
+
 const extractAccessToken = async (req) => {
   const { authorization } = req.headers;
   if (!authorization) {
@@ -16,15 +20,20 @@ const extractAccessToken = async (req) => {
 
 const extractRefreshToken = async (req) => {
   // get refresh token
-  const { refresh_token } = req.cookies;
-  if (!refresh_token) {
+  const refreshToken = req.cookies?.refresh_token;
+  if (!refreshToken) {
     throw createHttpError.Unauthorized();
   }
 
-  return refresh_token;
+  return refreshToken;
 };
 
-const verifyToken = async (token, key) => {
+/**
+ * Check token integrity
+ * @param {string} token - The token provided by user
+ * @param {string} key   - The secret key used to verify the token
+ */
+const verifyToken = (token, key) => {
   return JWT.verify(token, key, (err, decoded) => {
     if (err) {
       if (err.name === "JsonWebTokenError") {
@@ -40,10 +49,7 @@ const verifyToken = async (token, key) => {
 const verifyAccessToken = async (req, res, next) => {
   try {
     const accessToken = await extractAccessToken(req);
-    const payload = await verifyToken(
-      accessToken,
-      process.env.ACCESS_TOKEN_SECRET
-    );
+    const payload = verifyToken(accessToken, atKey);
 
     next(payload);
   } catch (err) {
@@ -54,10 +60,8 @@ const verifyAccessToken = async (req, res, next) => {
 const verifyAdminAccessToken = async (req, res, next) => {
   try {
     const accessToken = await extractAccessToken(req);
-    const payload = await verifyToken(
-      accessToken,
-      process.env.ACCESS_TOKEN_SECRET
-    );
+    const payload = verifyToken(accessToken, atKey);
+
     if (!payload.is_admin) throw new createHttpError.Forbidden();
     next();
   } catch (err) {
@@ -65,39 +69,40 @@ const verifyAdminAccessToken = async (req, res, next) => {
   }
 };
 
-const isRefreshTokenInWhiteList = async (tokenId, refreshToken) => {
-  const tokenInRedis = await redisClient.get(tokenId);
-
-  return tokenInRedis === refreshToken;
+/**
+ * Confirm the user refresh token in the whitelist.
+ * @param {string} tokenId - The userId gets from the payload of the decode.
+ * @param {string} refreshToken - The refresh token send by user
+ */
+const checkRefreshTokenInWhitelist = async (tokenId, refreshToken) => {
+  try {
+    const tokenInRedis = await redisClient.get(tokenId);
+    return tokenInRedis === refreshToken;
+  } catch (error) {
+    throw createHttpError.InternalServerError();
+  }
 };
 
 const verifyRefreshToken = async (req, res, next) => {
   try {
     const refreshToken = await extractRefreshToken(req);
-
-    const payload = await verifyToken(
-      refreshToken,
-      process.env.REFRESH_TOKEN_SECRET
-    );
+    const payload = verifyToken(refreshToken, rtKey);
 
     // Check refresh token in redis
-    const isTokenValid = await isRefreshTokenInWhiteList(
-      payload.user_id.toString(),
+    const isTokenValid = await checkRefreshTokenInWhitelist(
+      String(payload.user_id),
       refreshToken
     );
 
-    if (isTokenValid) {
-      next(payload);
-    } else {
-      throw new createHttpError.Unauthorized();
-    }
+    if (!isTokenValid) throw new createHttpError.Unauthorized();
+    next(payload);
   } catch (err) {
     next(err);
   }
 };
 
 export default {
-  verifyAccessToken,
-  verifyAdminAccessToken,
-  verifyRefreshToken,
+  verifyAccessToken: verifyAccessToken,
+  verifyAdminAccessToken: verifyAdminAccessToken,
+  verifyRefreshToken: verifyRefreshToken,
 };
